bf8fa2216471aab78c4afe9f814371869f3d1543
dev3robin <professionalrobin535@gmail.com>
2025-01-21 10:16:04 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+console.log("dasf")
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    
     <App />
   </StrictMode>,
 )
